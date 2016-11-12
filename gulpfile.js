a995f5c4f5c4ba970f72fe9628957691727514fe
Peter Vanhee <peter.vanhee@gmail.com>
2016-11-12 17:19:07 +0000

    Adapting the generator gulp files from the original scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,11 @@ var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
-gulp.task('static', function () {
-  // return gulp.src('**/*.js')
-  //   .pipe(excludeGitignore())
-  //   .pipe(eslint())
-  //   .pipe(eslint.format())
-  //   .pipe(eslint.failAfterError());
-});
-
 gulp.task('nsp', function (cb) {
   nsp({package: path.resolve('package.json')}, cb);
 });
 
-gulp.task('pre-test', function () {
-  return gulp.src('generators/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('test', ['pre-test'], function (cb) {
+gulp.task('test', [], function (cb) {
   var mochaErr;
 
   gulp.src('test/**/*.js')
@@ -38,7 +21,6 @@ gulp.task('test', ['pre-test'], function (cb) {
     .on('error', function (err) {
       mochaErr = err;
     })
-    // .pipe(istanbul.writeReports())
     .on('end', function () {
       cb(mochaErr);
     });
@@ -49,4 +31,4 @@ gulp.task('watch', function () {
 });
 
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+gulp.task('default', ['test']);
